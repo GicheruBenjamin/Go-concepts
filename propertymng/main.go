cda1a6547f81d39efdd9c0740ba0095d7389a9e0
GicheruBenjamin <benjaminnjuguna77@gmail.com>
2025-06-14 12:03:35 +0300

    feature: Start of property...

---
+++ propertymng/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello World")
+}
+
+/* 
+A Property has house/s 
+A house has  floor/s
+A floor has  room/s
+A room has  window/s and windows
+*/
