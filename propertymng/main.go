6b6c8036347199334aef1080222331707b8f58e9
GicheruBenjamin <benjaminnjuguna77@gmail.com>
2025-06-15 14:58:58 +0300

    feature: Grioten test...

--- propertymng/main.go
+++ propertymng/main.go
@@ -2,15 +2,55 @@ package main
 
 import (
 	"fmt"
+	"time"
+)
+
+type State string
+
+const (
+	Idle     State = "idle"
+	Running  State = "running"
+	Finished State = "finished"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	state := Idle
+
+	for {
+		switch state {
+		case Idle:
+			fmt.Println("Initializing system...")
+			state = startTask()
+		case Running:
+			fmt.Println("System is running...")
+			if taskDone() {
+				state = Finished
+			} else {
+				time.Sleep(time.Second)
+			}
+		case Finished:
+			fmt.Println("System is shutting down.")
+			cleanup()
+			return
+		default:
+			fmt.Println("Unknown state!")
+			return
+		}
+	}
 }
 
-/* 
-A Property has house/s 
-A house has  floor/s
-A floor has  room/s
-A room has  window/s and windows
-*/
+// Simulate starting a task
+func startTask() State {
+	fmt.Println("Starting task...")
+	return Running
+}
+
+// Simulate checking if task is done
+func taskDone() bool {
+	return time.Now().Second()%5 == 0 // simulate occasional finish
+}
+
+// Clean up resources
+func cleanup() {
+	fmt.Println("Cleaning up...")
+}
